a10c7508a5b9580c0451c52e7006ffeb2f50e54d
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2024-02-28 18:53:55 +0500

    create next app

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import * as LR from "@uploadcare/blocks";
+
+LR.registerBlocks(LR);
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-between">
+      <div className="max-w-7xl border h-full w-full">
+        <div>
+        </div>
+      </div>
+    </main>
+  );
+}
